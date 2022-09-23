94deff266a033905b598b496d358f900815d664d
Andrew Rivera <34843760+onthegocode@users.noreply.github.com>
2022-09-22 21:59:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,42 +9,41 @@ const anchor = eyeWhite.getBoundingClientRect();
 const anchorX = anchor.left + anchor.width / 2;
 const anchorY = anchor.top + anchor.height / 2;
 const angle = (cx, cy, ex, ey) =>
-  (Math.atan2(ey - cy, ex - cx) * 180) / Math.PI;
+	(Math.atan2(ey - cy, ex - cx) * 180) / Math.PI;
 
 const eye = (e) => {
-  const angleDeg = angle(e.clientX, e.clientY, anchorX, anchorY);
-  eyes.forEach((eye) => {
-    eye.style.transform = `rotate(${90 + angleDeg}deg)`;
-    eyeBrown.style.filter = `hue-rotate(${angleDeg}deg)`;
-  });
+	const angleDeg = angle(e.clientX, e.clientY, anchorX, anchorY);
+	eyes.forEach((eye) => {
+		eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+		eyeBrown.style.filter = `hue-rotate(${angleDeg}deg)`;
+	});
 };
 
 const blink = () => {
-  lid1.classList.add("lid-close", "transition");
-  lid2.classList.add("lid-close", "transition");
-  setTimeout(() => {
-    lid1.classList.remove("lid-close");
-    lid2.classList.remove("lid-close");
-  }, 500);
+	lid1.classList.add("lid-close", "transition");
+	lid2.classList.add("lid-close", "transition");
+	setTimeout(() => {
+		lid1.classList.remove("lid-close");
+		lid2.classList.remove("lid-close");
+	}, 500);
 };
 const freeEye = () => {
-  setInterval(() => {
-    document.removeEventListener("mousemove", eye);
-    let randX = Math.random() * 900 + 1;
-    let randY = Math.random() * 900 + 1;
-    eyes.forEach((eye) => {
-      const angleDeg = angle(randX, randY, anchorX, anchorY);
-      eye.style.transition = "all 1s ease";
-      eye.style.transform = `rotate(${90 + angleDeg}deg)`;
-      eyeBrown.style.filter = `hue-rotate(${Math.random() * 200 + 1}deg)`;
-    });
-  }, 800);
+	setInterval(() => {
+		document.removeEventListener("mousemove", eye);
+		let randX = Math.random() * 900 + 1;
+		let randY = Math.random() * 900 + 1;
+		eyes.forEach((eye) => {
+			const angleDeg = angle(randX, randY, anchorX, anchorY);
+			eye.style.transition = "all 1s ease";
+			eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+			eyeBrown.style.filter = `hue-rotate(${Math.random() * 200 + 1}deg)`;
+		});
+	}, 1000);
 };
 setInterval(blink, 10000);
+if (window.innerWidth <= 600) {
+	freeEye();
+}
 document.addEventListener("mousemove", eye);
 
 main.addEventListener("click", freeEye);
-
-if (window.innerWidth <= 600) {
-  freeEye();
-}
